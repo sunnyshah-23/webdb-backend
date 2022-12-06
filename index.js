58c45baef97b7f94546d8cdc8917271433346e35
Sunny Shah <sunnyshah@Sunnys-MacBook-Air.local>
2022-12-05 22:52:55 -0500

    mongoDB connect

--- index.js
+++ index.js
@@ -3,34 +3,30 @@ const path = require("path");
 const fs = require("fs");
 const { MongoClient } = require('mongodb')
 
-async function main() {
-    const uri = 'mongodb+srv://sunny23:sunny@cluster0.nvgw8zp.mongodb.net/?retryWrites=true&w=majority'
-    const client = new MongoClient(uri)
-    try {
-        await client.connect()
-        await listDatabase(client)
-        console.log("connected")
-    }
-    catch (e) {
-        console.log(e)
-    } finally {
-        await client.close()
-    }
-}
-main().catch(console.error)
+const uri =
+    "mongodb+srv://sunny23:sunny@cluster0.nvgw8zp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-async function listDatabase(client) {
-    // const databaseList = await client.db().admin().listDatabases()
-    // console.log(databaseList)
-    const res = await client.db("dev").collection("profile").find({})
-    let resArray = await res.toArray()
-    console.log(resArray)
+const connectDB = async () => {
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        console.log(" connection happened here")
+
+
+    } catch (e) {
+        console.error(e);
+    }
 }
-const server = http.createServer((req, res) => {
+
+connectDB();
+
+const server = http.createServer(async (req, res) => {
 
     const headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        'Content-Type': 'applicaiton/json'
     };
 
     if (req.url === '/') {
@@ -45,17 +41,15 @@ const server = http.createServer((req, res) => {
         );
     }
 
+    //fetch MongoDB data and send the response
     else if (req.url === '/api') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'project.json'), 'utf-8',
-            (err, content) => {
 
-                if (err) throw err;
-                // Please note the content-type here is application/json
-                res.writeHead(200, headers);
-                res.end(content);
-            }
-        );
+        const data = client.db("dev").collection("profile").find({});
+        const results = await data.toArray();
+
+        const profile = (JSON.stringify(results));
+        res.writeHead(200, headers);
+        res.end(profile);
 
     }
     else {
@@ -68,14 +62,8 @@ const server = http.createServer((req, res) => {
             }
         );
     }
-
-    /*
-
-        But what if we have  1000 pages/urls ? do we need to write 1000 if-else statements?
-
-    /*/
 });
 
-const PORT = process.env.PORT || 6959;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => console.log(`Great our server is running on port ${PORT} `));
